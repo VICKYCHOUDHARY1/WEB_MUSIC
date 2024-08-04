b54cd402bd475ba3c2e00f42dd90b5465db31978
𓆩𔘓𓆪 𝐕⊶𝐈⊶𝐊⊶𝐑⊶𝐀⊶𝐍⊶𝐓 𓆩𔘓𓆪 <166818922+vicky0604hello@users.noreply.github.com>
2024-08-04 14:51:08 +0530

    Update settings.py

--- YM/plugins/settings.py
+++ YM/plugins/settings.py
@@ -1,11 +1,29 @@
 import os
 from os import path
 
-DJANGO_SETTINGS_MODULE = 'YM.settings'
+import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'YM.plugins',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+"""DJANGO_SETTINGS_MODULE = 'YM.settings'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'"""
