17485487e71286c653409b1c0e2fd07f3c76f50b
vicky0604hello <choudharyji0604e@gmail.com>
2024-08-04 16:11:39 +0530

    extra pulgin removed

--- YM/plugins/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    # your other routes here
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
