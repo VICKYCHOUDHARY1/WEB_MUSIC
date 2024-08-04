b9fe3f3f8c6a4cae0cd47cf09634c8fc45c3a16b
𓆩𔘓𓆪 𝐕⊶𝐈⊶𝐊⊶𝐑⊶𝐀⊶𝐍⊶𝐓 𓆩𔘓𓆪 <166818922+vicky0604hello@users.noreply.github.com>
2024-08-04 21:23:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,12 @@ API_ID = getenv("API_ID", "25488022")
 API_HASH = getenv("API_HASH", "0c999a454fddd79251213be7944811e8")
 BOT_TOKEN = getenv("BOT_TOKEN", "6548314357:AAEFhj5_pAfTY9_lCsPkHvD_Zck4Xq8ibpU")
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", "-1002136795173")
+OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+#---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---x---
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/83da25751161cc4b9d408.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/59d3d5fbaca41ec09f33d.jpg")
+PLAYLIST_IMG_URL = "https://graph.org/file/954b70e8c68314875cb78.jpg"
+STATS_IMG_URL = "https://graph.org/file/dc565712c080a72b0320e.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/92349afcdfb9da6f7c693.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/92349afcdfb9da6f7c693.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/4cf64c2a1902b56a52d13.jpg"
